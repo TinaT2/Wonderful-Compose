1edffdf9327c8d0bb417e290955a423db9ae251e
Tina_TT2 <tina.t2aq@gmail.com>
2023-11-01 11:18:30 +0330

    Add chatFlexBox layoput for text messages

--- app/src/main/java/com/example/wonderfulcompose/ui/profile/Messenger.kt
+++ app/src/main/java/com/example/wonderfulcompose/ui/profile/Messenger.kt
@@ -2,12 +2,9 @@ package com.example.wonderfulcompose.ui.profile
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -22,11 +19,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.wonderfulcompose.R
 import com.example.wonderfulcompose.components.PreviewUtil
+import com.example.wonderfulcompose.components.chatflexbox.ChatFlexBoxLayout
 import com.example.wonderfulcompose.data.fake.catAdoptionDiscussion
 import com.example.wonderfulcompose.data.models.MessagePresenter
 
@@ -75,52 +71,19 @@ private fun MessengerInput(text: MutableState<String>, modifier: Modifier) {
         )
     }
 }
-
-//@Composable
-//fun TextMessageItem(body: String, userName: String, time: String) {
-//    Box(
-//        modifier = Modifier
-//            .padding(8.dp)
-//            .fillMaxWidth()
-//            .wrapContentHeight()
-//            .padding(8.dp)
-//    ) {
-//        Text(
-//            text = body,
-//            textAlign = TextAlign.Start,
-//            modifier = Modifier.align(Alignment.TopStart),
-//            overflow = TextOverflow.Visible
-//        )
-//        Text(
-//            text = time,
-//            textAlign = TextAlign.End,
-//            modifier = Modifier.align(Alignment.BottomEnd)
-//
-//        )
-//    }
-//}
 @Composable
 fun TextMessageItem(body: String, userName: String, time: String) {
-    Box(
+    ChatFlexBoxLayout(
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()
             .wrapContentHeight()
-            .padding(8.dp)
-    ) {
-        Text(
-            text = body,
-            textAlign = TextAlign.Start,
-            modifier = Modifier.align(Alignment.TopStart),
-            overflow = TextOverflow.Visible
-        )
-        Text(
-            text = time,
-            textAlign = TextAlign.End,
-            modifier = Modifier.align(Alignment.BottomEnd)
-
-        )
-    }
+            .padding(8.dp),
+        text = body,
+        messageStat = {
+            Text(text = time)
+        }
+    )
 }
 
 
